47fb4e32f773a8e8dda155d3c1fcb11a16c6db2a
mohammadrezakarimi <k.reza90@yahoo.com>
2022-08-28 18:32:59 +0430

    update

--- Book_Phone.py
+++ Book_Phone.py
@@ -26,10 +26,41 @@ def Home():
 	def Show():
 		con = sqlite3.connect('Phone_Book.db')
 		cur = con.cursor()
-		cur = cur.execute("SELECT Name FROM Book")
+		cur = cur.execute("SELECT * FROM Book")
 		text = cur.fetchall()
-		cur = cur.execute("SELECT Phone FROM Book")
-		phone = cur.fetchall()
+		with open('Phone_Book.html', 'w',encoding='utf-8') as file:
+			file.write(str(text))
+		messagebox.showinfo('Show','You Can See All User in Phone_Book.html')
+		
+	def delete():
+		messagebox.showinfo("Delete","Enter Full Name")
+		Toplevel = Tk()
+		Toplevel.title('Delete')
+		Toplevel.resizable(False,False)
+		lbl4 = ttk.Label(Toplevel,text='Delete',font=('arial',15))
+		lbl4.pack()
+		def delete():
+			d = e3.get()
+			con = sqlite3.connect('Phone_Book.db')
+			cur = con.cursor()
+			cur.execute("DELETE FROM Book WHERE name = ?",(d,))
+			con.commit()
+			Toplevel.destroy()
+			messagebox.showinfo("Delete","Delete Successfully")
+			con = sqlite3.connect('Phone_Book.db')
+			cur = con.cursor()
+			cur = cur.execute("SELECT * FROM Book")
+			text = cur.fetchall()
+			with open('Phone_Book.html', 'w',encoding='utf-8') as file:
+				file.write(str(text))
+		e3 = Entry(Toplevel,font=('arial',15))
+		e3.pack()
+
+		btn = Button(Toplevel,text='Delete',command=delete)
+		btn.pack(pady=5)
+		Toplevel.mainloop()
+
+		'''
 		for t in text:
 			pass
 		for tt in phone:
@@ -38,8 +69,9 @@ def Home():
 			file.write(str("\n%s\n" % t + ":"))
 			file.write(str("\n%s\n" % tt))
 		messagebox.showinfo('Show','You Can See All User in Phone_Book.html')
-	
-	
+		'''
+	def update():
+		pass
 	window = Tk()
 	window.title('Phone Book')
 	window.geometry('300x300')
@@ -53,7 +85,7 @@ def Home():
 	lbl1 = Label(window,image=Photo)
 	lbl1.pack()
 
-	lbl2 = Label(window,text='Name and Lastname',font=('arial',15))
+	lbl2 = Label(window,text='Full Name',font=('arial',15))
 	lbl2.pack()
 
 	e1 = Entry(window,font=('arial',15))
@@ -64,7 +96,6 @@ def Home():
 
 	e2 = Entry(window,font=('arial',15))
 	e2.pack()
-
 
 	btn = Button(window,text='Add',font=('arial',15),command=add)
 	btn.pack(pady=20)
@@ -79,8 +110,8 @@ def Home():
 	menubar.add_cascade(label="Help",menu=filemenu1)
 
 	filemenu.add_command(label='Show',command=Show)
-	filemenu.add_command(label='Update')
-	filemenu.add_command(label='Delete')
+	filemenu.add_command(label='Update',command=update)
+	filemenu.add_command(label='Delete',command=delete)
 	filemenu.add_command(label='Exit',command=exit)
 	filemenu1.add_command(label='About',command=About)
 
